4588c7f5ed3c2151ea568e05c056b9d5a6ad9823
youngjin jung <youngjinjung@youngjinui-MacBookPro.local>
2026-04-04 10:30:23 +0900

    fix: add middleware to outputFileTracingIncludes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next'
 
 const config: NextConfig = {
   experimental: { serverActions: { allowedOrigins: ['localhost:3000'] } },
+  outputFileTracingIncludes: { "/middleware": ["./middleware.ts"] },
   images: { remotePatterns: [{ protocol: 'https', hostname: '*.supabase.co' }] },
 }
 
